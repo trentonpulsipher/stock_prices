cf1f204161121e0c6f4d3dcd5072c24330236104
Pulsipher <trenton.pulsipher@tpulsipher17877.lan>
2018-10-15 06:46:36 -0600

    add tabs to view

--- stock_prices/app.R
+++ stock_prices/app.R
@@ -20,7 +20,7 @@ allTickers <- read_csv("secwiki_tickers.csv") %>%
 
 
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- fluidPage(
    # App title
    titlePanel("Stock Prices"),
@@ -34,14 +34,18 @@ ui <- fluidPage(
          tableOutput("tickerInfo"),
          textOutput("description")
       ),
-      # Show rBokeh plot of time-series
+      # Show rBokeh plot of time-series and Forecast and Data
       mainPanel(
-         rbokehOutput("tsPlot")
+        tabsetPanel(
+          tabPanel("Historical", rbokehOutput("tsPlot")),
+          tabPanel("Short-term Forecast", rbokehOutput("forecast")),
+          tabPanel("Data", DT::dataTableOutput("dataTable"))
+        )
       )
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to render plots/tables
 server <- function(input, output) {
   output$tickerInfo <- renderTable(allTickers %>% 
                                     filter(Ticker == input$ticker) %>%
@@ -49,27 +53,48 @@ server <- function(input, output) {
   output$description <- renderText(
     "DESCRIPTION of WIKI data\nhttps://www.quandl.com/databases/WIKIP\nEnd of day stock prices, dividends and splits for 3,000 US companies, curated by the Quandl community and released into the public domain."
   )
+  # grab the raw data from Quandl
+  data <- reactive({
+    get_raw_data(input$ticker) %>%
+    as.tibble() %>%
+    slice(-1) %>% # remove the header line
+    mutate(
+      Date = ymd(unlist(lapply(str_split(value, ","), "[[", 1))),
+      Close = as.numeric(unlist(lapply(str_split(value, ","), "[[", 5))),
+      Volume = as.numeric(unlist(lapply(str_split(value, ","), "[[", 6)))
+    ) %>%
+    select(Date, Close, Volume) %>%
+    arrange(desc(Date))
+  })
 
-   output$tsPlot <- renderRbokeh({   
-     # grab the raw data from Quandl
-     get_raw_data(input$ticker) %>%
-       as.tibble() %>%
-       slice(-1) %>% # remove the header line
-       mutate(Date = ymd(unlist(lapply(str_split(value, ","), "[[", 1))),
-              Close = as.numeric(unlist(lapply(str_split(value, ","), "[[", 5)))) %>%
-       figure(xlab = "", ylab = "Price at Closing (Daily)") %>%
-       ly_points(Date, Close, hover = list("Date" = Date, "Closing Price" = Close))
-   })
+  output$tsPlot <- renderRbokeh({
+    data() %>%
+      figure(xlab = "", ylab = "Price at Closing (Daily)") %>%
+      ly_points(Date, Close, hover = list("Date" = Date, "Closing Price" = Close))
+  })
+  output$forecast <- renderRbokeh({
+    data() %>%
+      filter(Date > (max(Date, na.rm = T) - months(3))) %>%
+      figure(xlab = "", ylab = "Price at Closing (Daily") %>%
+      ly_points(Date, Close, hover = list("Date" = Date, "Closing Price" = Close))
+  })
+  output$dataTable <- DT::renderDataTable({
+    tmp <- data()
+    DT::datatable(tmp)
+  })#, options = list(pageLength = 10)))
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
 
 # library(rsconnect)
-# rsconnect::deployApp('~Documents/Development/R/stock_prices/stock_prices')
+# rsconnect::deployApp('~/Documents/Development/R/stock_prices/stock_prices')
 
 ## NOTES:
 # https://www.quandl.com/databases/WIKIP
 # DESCRIPTION of WIKI data: End of day stock prices, dividends and splits for 3,000 US companies, curated by the Quandl community and released into the public domain.
 # could vary more than just closing price
 # could provide shorter window (say last year) if desired
+# columns of data as it comes from Qunadl: c("Date", "Open", "High", "Low", "Close", "Volume", "Ex-Dividend", "Split Ratio", 
+# "Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume")
+
